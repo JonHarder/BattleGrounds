74adf7a650fcdc29e68aa6ba9428606c12aa3495
drewmaas <maas3324@gmail.com>
2016-03-20 00:01:49 -0500

    Movement Updates
    Trying to get the sprite to move to where i click instead of just vector
    right. Input of the click is written.

--- Movement_script.gd
+++ Movement_script.gd
@@ -12,9 +12,16 @@ func _ready():
 	# this means setting process to true.
 	set_process(true)
 
+func _input(event):
+   # if user left clicks
+   if(event.type == InputEvent.MOUSE_BUTTON):
+      if(event.button_index == 1):
+         self.set_pos(Vector2(event.x,event.y)) 
+
 
 func _process(delta):
    if(Input.is_action_pressed("MOVE_HORIZONTAL")):
       var cur_pos = self.get_pos()
-      cur_pos.x += 1
+      cur_pos.x += .5
       self.set_pos(cur_pos)
+
