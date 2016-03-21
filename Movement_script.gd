8894ddc04064660bfc20f7afc2b6344662c60195
Jon Harder <jonharder6@gmail.com>
2016-03-20 20:39:16 -0500

    fixed movement script

--- Movement_script.gd
+++ Movement_script.gd
@@ -10,22 +10,11 @@ func _ready():
 	# Initialization here
 	#need to tell godot you want to process each cycle.
 	# this means setting process to true.
-	set_process(true)
+	# set_process(true)
+	set_process_input(true)
 
 func _input(event):
    # if user left clicks
    if(event.type == InputEvent.MOUSE_BUTTON):
       if(event.button_index == 1):
          self.set_pos(Vector2(event.x,event.y)) 
-
-
-
-
-func _process(delta):
-    # if user left clicks
-   if(event.is_action("MOVE_HORIZONTAL")):
-         var cur_pos = self.get_pos()
-         cur_pos = (Vector2(event.x,event.y)) 
-         self.set_pos(cur_pos)
-                  
-
