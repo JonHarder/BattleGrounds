5e3ccb6214e858589c7c84f65d22c5b19e78eb6f
Jon Harder <jonharder6@gmail.com>
2016-03-21 21:31:44 -0500

    added movement script to dude
    can move freely left and right. collides with map and falls due to
    gravity

---
+++ dude.gd
@@ -0,0 +1,26 @@
+
+extends KinematicBody2D
+
+var speed = 200
+var gravity = 3.0
+var TERMINAL_VEL = 50
+var dy = 0
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_fixed_process(true)
+	
+func _fixed_process(delta):
+	if(Input.is_action_pressed("MOVE_LEFT")):
+		move(Vector2(-speed*delta,0))
+	if(Input.is_action_pressed("MOVE_RIGHT")):
+		move(Vector2(speed*delta, 0))
+
+	# figure out how to count collisions only for bottom edge
+	if(!is_colliding()):
+		if(dy < TERMINAL_VEL):
+			dy += gravity*delta	
+	else:
+		dy = 0
+	move(Vector2(0,dy))
